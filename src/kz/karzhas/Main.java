ecd08c04db043cef4cf6f4a505474e165d1052c2
Karzhas Tokbolatov <tokbolatov.k@gmail.com>
2021-11-19 10:11:39 +0600

    first commit

---
+++ src/kz/karzhas/Main.java
@@ -0,0 +1,9 @@
+package kz.karzhas;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Я ПЕРЕДАЛ ЭТОТ КОД ИЗ ЛОКАЛЬНОГО РЕПОЗИТОРИЯ!!!!");
+        System.out.println("ПРИВЕТ МИР");
+    }
+}
