1ff48c7f03d9f7fedc77d61a4a971bebf50d246a
cowbuno <talipov.zhenis@gmail.com>
2021-11-19 10:44:50 +0600

    privet

--- src/kz/karzhas/Main.java
+++ src/kz/karzhas/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Я ПЕРЕДАЛ ЭТОТ КОД ИЗ ЛОКАЛЬНОГО РЕПОЗИТОРИЯ!!!!");
         System.out.println("ПРИВЕТ МИР");
+        System.out.println("Privet ot Zhenisa");
     }
 }
